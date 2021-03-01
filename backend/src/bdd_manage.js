e84f1adca3cb7a13d6b012f7f661f7fd6b506b31
Aurélien <robineau.aure@gmail.com>
2021-03-01 15:54:39 +0100

    Fix bug

--- backend/src/bdd_manage.js
+++ backend/src/bdd_manage.js
@@ -6,8 +6,8 @@ export default db = {
 
     initDatabase : function (){
 
-        this.password_db = process.env.DB_USER
-        this.login_db = process.env.DB_PASSWORD
+        this.password_db = process.env.DB_PASSWORD
+        this.login_db = process.env.DB_USER
         this.db_name = process.env.DB_NAME
 
         this.uri = `mongodb+srv://${this.login_db}:${this.password_db}@marche-libre.nkkht.mongodb.net/${this.db_name}?retryWrites=true&w=majority`
