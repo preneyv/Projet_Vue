297bec87dcca431dbcc5c058713474b720524543
Aurélien <robineau.aure@gmail.com>
2021-03-01 14:45:55 +0100

    Load config from .env

--- backend/models/bdd_manage.js
+++ backend/models/bdd_manage.js
@@ -1,13 +1,14 @@
 import mongoose from 'mongoose'
+import process from 'process'
 
 const db = {
 
     initDatabase : function (){
 
-        this.password_db = "valere"
-        this.login_db = "valere-admin"
+        this.password_db = process.env.DB_USER
+        this.login_db = process.env.DB_PASSWORD
+        this.db_name = process.env.DB_NAME
 
-        this.db_name = "marche_libre"
         this.uri = `mongodb+srv://${this.login_db}:${this.password_db}@marche-libre.nkkht.mongodb.net/${this.db_name}?retryWrites=true&w=majority`
         this.connect(this.uri)
     },
