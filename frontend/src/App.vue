2464412af10e17aaaa7de72a6b5221f101eef7a2
Hippolyte Soulier <hi.soulier@gmail.com>
2021-03-02 14:59:10 +0100

    Navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,15 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
- </template>
+    <Navbar />
+    <router-view />
+</template>
 
-
-<style lang="scss">
-
-body {
-  background-color: $background-color;
-  color: $text-color;
-  font-family: $font;
+<script>
+import Navbar from "@/components/Navbar.vue"
+export default {
+    components: {
+        Navbar,
+    },
 }
+</script>
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style lang="scss"></style>
---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,44 @@
+<template>
+    <nav class="navbar">
+        <div class="navbar__container">
+            <div class="navbar__logo">
+                <router-link to="/">
+                    <img src="../assets/logo.png" alt="Logo Vue" />
+                </router-link>
+            </div>
+            <ul class="navbar__links">
+                <li class="navbar__link">
+                    <router-link to="/projects">Projet</router-link>
+                </li>
+                <li class="navbar__link">
+                    <router-link to="/about">A propos</router-link>
+                </li>
+                <li class="navbar__link">
+                    <router-link to="/submit">Soumettre un projet</router-link>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: "Navbar",
+}
+</script>
+
+<style lang="scss" scoped>
+.navbar {
+    &__container {
+        margin-top: space(2);
+        width: width(10);
+        @include flex(row, space-between);
+        @extend .mx-auto;
+    }
+    &__links {
+      @include flex();
+      display: inline-flex;
+      
+    }
+}
+</style>
