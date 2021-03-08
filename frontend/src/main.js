b0bf080c01969e36ce18ab51bb65802f88490b51
Aurélien <robineau.aure@gmail.com>
2021-03-08 19:36:55 +0100

    Add MultiSelect component

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCheck } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-createApp(App).use(router).mount('#app')
+library.add(faCheck)
+
+const Vue = createApp(App)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.use(router).mount('#app')
