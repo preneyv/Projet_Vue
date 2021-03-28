38c3e7001910c3734e2d90a7626d8345ae191a77
Aurélien <robineau.aure@gmail.com>
2021-03-28 19:40:21 +0200

    Revert

--- frontend/src/views/projects/Submit.vue
+++ frontend/src/views/projects/Submit.vue
@@ -73,34 +73,28 @@
 			</div>
 
 			<div class="submit-project__input-container">
-				<div
-					v-for="profil, i in neededProfils"
-					:key="i"
-					class="submit-project__needed-profil">
+				<div class="submit-project__needed-profil">
 					<BaseSelect
-						:id="`project-needed-profil-type-${i}`"
-						:name="`needed-profil-type-${i}`" 
+						id="project-needed-profil-type-0"
+						name="needed-profil-type-0" 
 						label="Profils recherchés"
-						:items="profilTypesForSelect"
-						:value="profilTypes[profil.type] || null"
-						@change="e =>handleProfilTypeChange(e, i)"
+						:items="profilTypes"
 						allowSearch
 					/>
 					<BaseInput
 						type="number"
-						:id="`project-needed-profil-number-${i}`"
-						:name="`needed-profil-number-${i}`" 
+						id="project-needed-profil-number-0"
+						name="needed-profil-number-0"
 						:min="0"
 					/>
 					<BaseSelect
-						:id="`project-needed-profil-skills-${i}`"
-						:name="`needed-profil-skills-${i}`" 
-						:items="profilTypes[profil.type]?.skills || []"
+						id="project-categories"
+						name="categories"
+						:items="profilTypes[0].skills"
 						allowSearch
 						multiple
 					/>
 				</div>
-				<button type="button" @click="addProfil">Ajouter un profil</button>
 			</div>
 		</form>
 	</div>
@@ -123,28 +117,8 @@ export default {
 		BaseTextarea
 	},
 	data() {
-		return {
-			licenses,
-			categories,
-			officialLinkTypes,
-			profilTypes,
-			profilTypesForSelect: Object.keys(profilTypes).map(key => ({ value: key, ...profilTypes[key] })),
-			neededProfils: []
-		}
-	},
-	methods: {
-		addProfil() {
-			this.neededProfils.push({
-				type: 'developer',
-				number: 0,
-				skills: []
-			})
-		},
-		handleProfilTypeChange(e, index) {
-			console.log(e.target.value, index)
-			this.neededProfils[index].type = e.target.value
-		}
-	},
+		return { licenses, categories, officialLinkTypes, profilTypes }
+	}
 }
 </script>
 
