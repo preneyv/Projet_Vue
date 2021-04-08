9fbc860ffc17b482b9a4186001c9044cf468a977
Aurélien <robineau.aure@gmail.com>
2021-04-08 10:13:56 +0200

    Remove useless lines

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,15 +5,17 @@ import router from './router'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faCheck } from '@fortawesome/free-solid-svg-icons'
 import { faTimes } from '@fortawesome/free-solid-svg-icons'
+import { faCaretUp } from '@fortawesome/free-solid-svg-icons'
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 library.add(faCheck)
 library.add(faTimes)
-
+library.add(faCaretUp)
+library.add(faCaretDown)
 
 const Vue = createApp(App)
+
 Vue.component('font-awesome-icon', FontAwesomeIcon)
-
-
 
 Vue.use(router).mount('#app')
